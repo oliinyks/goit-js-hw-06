8e24523bf2488d7a84329c7bdd9971797e647caa
Olijnyks <olijnyks@ukr.net>
2022-07-20 19:47:52 +0300

    add 3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,11 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryEl = document.querySelector(".gallery");
+
+const markup = images.map(
+	(image) => `<li><img src="${image.url}" alt="${image.alt}"></li>`
+).join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', markup);
