032dd969d3ba51751b1775f6c18a7530331164e2
Olijnyks <olijnyks@ukr.net>
2022-07-21 09:34:04 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,8 @@ const validationInputEl = document.querySelector("#validation-input");
 validationInputEl.addEventListener('blur', (event) => {
 
 	if (event.currentTarget.value.length === 6) {
-    return validationInputEl.classList.add("valid");
+		 validationInputEl.classList.remove("invalid");
+		return validationInputEl.classList.add("valid");
 	}
 	return validationInputEl.classList.add("invalid");
 })
