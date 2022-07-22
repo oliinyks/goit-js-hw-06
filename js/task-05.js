56db2e069649b5937e21ed0afe64987ecc468ad8
Olijnyks <olijnyks@ukr.net>
2022-07-22 09:35:56 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,11 @@
 const inputEl = document.querySelector('#name-input')
 const userNameEl = document.querySelector('#name-output')
 
-inputEl.addEventListener(
-    'input',
-    (event) => (userNameEl.textContent = event.currentTarget.value)
-)
+inputEl.addEventListener('input', (event) => {
+    const newUserName = event.currentTarget.value
+
+    if (newUserName === '') {
+        return (userNameEl.textContent = 'Anonymous')
+    }
+    return (userNameEl.textContent = newUserName)
+})
